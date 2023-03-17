9aba31cca794ddec972f346f39407013e83016ba
Lavender <lavenderwork1@gmail.com>
2023-03-17 16:14:42 +0800

    Updata

--- WeHelp_Bootcamp/Application_Form/js/main.js
+++ WeHelp_Bootcamp/Application_Form/js/main.js
@@ -1,9 +1,14 @@
 const main = document.getElementById("main");
 
 function showContent() {
-    var deviceHeight = window.screen.height;
+    // var deviceHeight = window.screen.height;
+    var deviceHeight = window.innerHeight;
     main.style.display = "block";
     main.style.height = deviceHeight + "px";
 
     main.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 }
+
+// window.addEventListener("click", function () {
+//     element.scrollIntoView();
+//   });
