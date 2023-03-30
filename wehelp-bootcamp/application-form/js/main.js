4e9cd22533451a786274eda2370da5022f811d28
Lavender <lavenderwork1@gmail.com>
2023-03-30 15:46:41 +0800

    updata

---
+++ wehelp-bootcamp/application-form/js/main.js
@@ -0,0 +1,56 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mainEl = document.querySelector('#main');
+    const postCntEl = document.querySelector('#post .container');
+
+    const linkScrollTo = (event, el) => {
+        event.preventDefault();
+
+        let deviceHeight = window.innerHeight;
+
+        mainEl.classList.remove('hidden');
+        if (deviceHeight > mainEl.offsetHeight) {
+            mainEl.style.height = deviceHeight + 'px';
+        } else {
+            mainEl.style.height = mainEl.offsetHeight + 'px';
+        }
+
+        postCntEl.querySelectorAll('.content').forEach((el) => {
+            el.classList.add('hidden');
+        });
+
+        let targetPost = document.querySelector(el.hash);
+        targetPost.classList.remove('hidden');
+        targetPost.style.opacity = 0;
+        targetPost.style.transition = 'opacity 1s ease-in-out';
+
+        if (deviceHeight > targetPost.offsetHeight) {
+            postCntEl.style.height = deviceHeight + 'px';
+        } else {
+            postCntEl.style.height = 'auto';
+        }
+
+        mainEl.style.height = 'auto';
+
+        window.setTimeout(() => {
+            targetPost.style.opacity = 1;
+        });
+
+        mainEl.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+    }
+
+    const navLinkEls = document.querySelectorAll('.nav-link');
+    const heroLinkEls = document.querySelectorAll('.hero-link');
+
+    heroLinkEls.forEach((el) => {
+        el.addEventListener('click', function(event) {
+            linkScrollTo(event, this);
+        });
+    });
+
+    navLinkEls.forEach((el) => {
+        el.addEventListener('click', function(event) {
+            linkScrollTo(event, this);
+        });
+    });
+});
